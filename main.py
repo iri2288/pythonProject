6fa698568c90a8656ea82ec9b0d57791c5088113
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-16 21:29:17 +0300

    For more

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import turtle as t
-colors = ['red', 'green', 'blue',
-          'yellow', 'gray', 'white']
-t.bgcolor('black')
-t.speed(0)  # максимальная скорость
-angle = 360 / len(colors) - 1
+for i in range(101):
+    if i % 10 == 5:
+        print(i)
 
-for x in range(200):
-    t.pencolor(colors[x % len(colors)])
-    # t.pencolor(colors[x % len(colors[0:2])])
-    t.width(x // 100 +1)
-    t.forward(x)
-    t.left(angle)
-t.mainloop()
+a = list()  # пустой список с помощью функции list
 
+for i in range(101):
+    if i % 10 == 5:
+        a.append(i)
+
+print(*a, sep=', ')  # * передача аргумента в виде списка
 
