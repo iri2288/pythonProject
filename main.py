03c151ff2b981c769fef3960743d46a987351d33
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-16 21:04:20 +0300

    For loop, Range only even

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-a = []
+a = ['Первый', 'Второй']
+members = 10
+
+for x in (range(members)):
+    print(a[x % 2])
+
+print('Расчет окончен')
+
 
 for x in range(11):
     a.append(x)
 
-for x in reversed(range(11)):
-    a.append(x)
-
-print(a)
+for index in range(0, len(a),2):
+    print(a[index])
 
 
