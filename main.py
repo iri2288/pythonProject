49cf7b9837b35df01bd5e144800489da01df5408
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-18 21:29:41 +0300

    Strip

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print(string)
 
 print('И ', end='')
 print(*lst, sep=', и ', end='.')
+
+# strip удаляет элементы в начале и конце строки
+r = 'ротор'
+print(r.strip('р'))
+print(r.rstrip('р')) # удаляет только справа
+print(r.lstrip('р')) # удаляет только слева
