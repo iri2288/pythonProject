31d15a21f1c5ecafc2d71a45305ff0dce256ba49
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-18 21:09:09 +0300

    Strip split join count

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ print(dir(''))
 # join
 # count
 
-s = 'работа'
-f = 'а'
-if f in s:
-    print('буква', f, 'есть в слове', s)
-    print('и она встречается', s.count(f), 'раз(а)')
-else:
-    print('Буквы', f, 'в слове', s, 'нет!')
+s = 'Мама мыла раму'
 
-print('C:\\Windows\\Fonts') # запись пути
+lst = s.split()  # пробел по умолчанию
+print(type(lst))
+
+string = ', ' .join(lst)
+
+print(string)
+
+lst = ['Козерог', 1]  # Козерог -1
+# print(lst[0])
+
+# new_lst = list(map(str, lst))
+#print(new_lst)
+# print('-'.join(list))
+
+print('-'.join(map(str, lst)))
+# print(str(num)[::-1])
+# num = list(num)
+# print(num)
+
+
