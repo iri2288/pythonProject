d19d77aa7078a66004ee2a1faf1f2ff81f0fba8e
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-18 19:36:02 +0300

    If elif else and time sample

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-a = list(input('Ввод числа:'))
+from datetime import datetime as d
 
-for n in a:
-    if int(n) % 2 == 0:
-        print('Число:', n, 'четное.')
-    else:
-        print('Число:', n, 'нечетное.')
+raw_time = d.time(d.now())
+
+# строка форматирования времени - strftime
+# %H - часы в 24 часовом формате
+# %M - часы в 24 часовом формате
+# %S - часы в 24 часовом формате
+
+print('на часах', raw_time.strftime('%H:%M'))
+
+cur_hour = int(raw_time.strftime('%H'))
+
+if cur_hour >= 6 and cur_hour < 12:
+    print('Доброе утро')
+elif cur_hour >= 12 and cur_hour < 18:
+    print('Добрый день')
+elif cur_hour >= 18 and cur_hour < 23:
+    print('Добрый вечер')
+else:
+    print('Доброй ночи')
