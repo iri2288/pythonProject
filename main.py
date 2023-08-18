cc848f712b69052f7ed541ed403069e5889ddd61
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-18 21:23:18 +0300

    Join and print comparison

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-print(dir(''))
-# Методы строк
-# strip
-# split
-# join
-# count
+lst = ['божество', 'вдохновенье', 'смех', 'слезы', 'любовь']
 
-s = 'Мама мыла раму'
-
-lst = s.split()  # пробел по умолчанию
-print(type(lst))
-
-string = ', ' .join(lst)
-
+# print('И', ', и '.join(lst))
+string = 'И ' + ', и '.join(lst) + '.'
 print(string)
 
-lst = ['Козерог', 1]  # Козерог -1
-# print(lst[0])
-
-# new_lst = list(map(str, lst))
-#print(new_lst)
-# print('-'.join(list))
-
-print('-'.join(map(str, lst)))
-# print(str(num)[::-1])
-# num = list(num)
-# print(num)
-
-
+print('И ', end='')
+print(*lst, sep=', и ', end='.')
