9a8530ac6d8bac8abe017efab46b896b5e98ceb0
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-23 20:39:43 +0300

    Dictionary generation from keys

--- main.py
+++ main.py
@@ -1,55 +1,6 @@
 # словари
 # список index - value
 # словарь key - value
-d = dict()  # создание пустого словаря
-d = {}  # создание пустого словаря
-d = {
-    'стол': 'table',
-    'стул': 'chair',
-}
+d = dict.fromkeys([1, 2, 3, 4, 5], '1')
 
-d['вишня'] = 'cherry'
-
-d = {
-    'well': ['хорошо', 'колодец'],
-    'блюдо' : 'окрошка'
-    'состав': ['квас', 'яйцо', 'огурец', 'колбаса']
-}
-
-for item in d:
-    if isinstance(type(d[item] == list))
-        print(item, end=':\n')
-        for v in d[item]:
-        print('\t', v)
-    else:
-        print(item, d[item])
-
-print(d['стол'])
-
-for item in d:  # перебор словаря по ключам
-    if isinstance(type(d[item] == list))
-        d[item].sort()  # сортируем список перед выводом
-        print(item, end=':\n')
-        for v in enumerate(d[item)]:
-        print('\t' + str(i+1) + '.', v)
-    else:
-        print(item, d[item], sep=': ')
-
-
-
-print(d['стол'])
-print(list(d.keys())) # список ключей
-print(list(d.values())) # список значений
-print(d.items())
-# выясним есть ли слово "кот" среди ключей
-if 'кот' in d.keys(): # метод keys - ключи
-    print('Кот есть')
-
-if 'cat' in d.values(): # метод values - значения
-    print('Cat есть')
-
-for x, y in d.items():
-    print(x, y, sep='->')
-
-
-
+print(d)
