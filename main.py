d0539f1adeac5bef0f85837edcefd1f2677fde9b
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-23 20:49:46 +0300

    Dictionary generation from 2 lists

--- main.py
+++ main.py
@@ -4,3 +4,16 @@
 d = dict.fromkeys([1, 2, 3, 4, 5], '1')
 
 print(d)
+
+# используя два списка
+keys = ['one', 'two', 'three']
+values = ['один', 'два', 'три']
+d = dict() # создан пустой словарь
+l = len(keys)
+
+# for i in range(l):
+#    d.keys[i] = values[i]
+
+d = {k: v for k, v in zip(keys, values)}  # генерация словаря с помощью zip
+
+print(d)
