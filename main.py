657364850b456c208dc8db66e33d5aec2cacc51d
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-25 21:21:18 +0300

    Lambda function

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-# Аннотированние типов
+# Анонимные функции лямбда - функции (lambda)
 
-def summ(a: int, b: int) -> int:  # аннотирование типов
-    return a + b
 
-print(summ(4, 6))
-print(summ(4.5, 6.5))
-print(summ('a','b'))
+def power(a, b):
+    return a ** b
+
+my_power = lambda a, b: a ** b
+print(my_power(2,4))
+
+lst = [1, 2, 3, 4, 5]
+
+lst2 = map(lambda a: a * 2, lst)
+print(list(lst2))
+
+lst = ['Иван', 'Петр', 'Сидор']
+
+lst_name = map(lambda n: 'Привет, ' + n, lst)
+print(list(lst_name))
+
+lst = ['Иван', 'Петр', 'Сидор']
+lst.sort(reverse=True, key=lambda x: x[2])
+print(lst)
