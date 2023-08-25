01056a5b75785fb4945fdd00963ae5e6251b5a1a
Giza <131477475+Toccle@users.noreply.github.com>
2023-08-25 20:48:13 +0300

    Args and kwargs

--- main.py
+++ main.py
@@ -1,77 +1,35 @@
-# Функции, количество аргументов должно совпадать, последовательность аргументов имеет значение
-# Область внутри функции local scope
-# Функция по умолчанию не имеет права работать с внешней глобальной областью данных,только если специально задать global
-def greeting(name) -> str:  # определение типа
-    # print('Привет', name)
-    return 'Привет, ' + str(name)
+# Функции с переменным числом аргументов
+# * сообщает интерпритатору что кол-во позиционных аргументов будет переменным >0 передается кортеж
+# ** сообщает интерпритатору что кол-во именованных аргументов будет переменным >0 передается словарь
+def var_args(*args):
+    """
+    :param args: только целые числа,
+    :return: кортеж - число аргументов и сумму как целое число
+    """
+    summ = 0
+    for arg in args:
+        summ += arg
+    # return(var_args(1, 2, 3, 4, 5)
+    return len(args), summ, args  # возврат нескольких значений кортеж
 
-result = greeting('Tim')
-# greeting('Тимоша')
-print(result)
-
-def greeting(name, surname, age):
-    # print('Привет', name)
-    string = 'Привет, ' + str(name) + ' ' + str(surname) + '!'
-    string += '\nТебе' + ' ' + str(age) + ' ' + 'лет'
-    return string
-
-result = greeting('Tim', 'Cat', 2)
-print(result)
-
-def greeting(name='Noname', surname='Nosurname', age=0):  # функция стала универсальной, если в первом аргументе задан
-# параметр, остальные параметры также должны быть заданы
-    # print('Привет', name)
-    string = 'Привет, ' + str(name) + ' ' + str(surname) + '!'
-    if age != 0:
-        string += '\nТебе' + ' ' + str(age) + ' ' + 'лет'
-    return string
-
-result = greeting('Tim', 'Cat', 2)
-result = greeting(surname='Tim', name='Cat')  # для изменения последовательности аргументов, вызов функции с именованным
-# аргументами
-print(result)
-
-def greeting(name=' ', surname=' ', age=0, sex=' '):  # функция стала универсальной, если в первом аргументе задан
-# параметр, остальные параметры также должны быть заданы
-    # print('Привет', name)
-    string = 'Привет, ' + str(name) + ' ' + str(surname) + '!'
-    if age != 0:
-        string += '\nТебе' + ' ' + str(age) + ' ' + 'лет'
-    if sex:
-        string+=  '\nПол' + sex
-    return string
-
-result = greeting('Tim', 'Cat', 2)
-result = greeting(surname='Tim', name='Cat')  # для изменения последовательности аргументов, вызов функции с именованным
-# аргументами
-print(result)
-
-def incr(number):
-    # local scope
-    return number + 1
-
-# global scope
-num = 5 # глобальная переменная
-print(incr(num))
-
-def incr():
-    # local scope
-    global num  #
-    num +=1
-    return num
-
-# global scope
-num = 5 # глобальная переменная
-print(incr())
-print(num)
+gnt, summa, lst = var_args(1, 2, 3, 4, 5)
+print('Число аргументов:', gnt)
+print('Сумма:', summa)
+print('Числа на входе:', *lst)
 
 
-def even_or_odd(number):
-    if number % 2 == 0:
-        return 'Четное'
-    # else: не сработает т.к. функция закончится на return в условии if
-    return 'Нечетное'
+def named_args(**kwargs):  # keyword именованные
+    print(kwargs)
 
-num = 6
-print(even_or_odd(num))
 
+# вызов named_args (key-word)
+named_args(first='первый', second='второй', third='третий')  # создается словарь
+
+
+def any_func_refer(n):
+    n[0] = 0
+    print(n)
+
+
+array = [5, 25, 625]
+any_func_refer(array)
