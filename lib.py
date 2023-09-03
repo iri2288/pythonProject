49a5b64ffd5587a9e9ae1762471b1d40ab83a18e
iri2288 <133140596+iri2288@users.noreply.github.com>
2023-09-03 16:37:07 +0300

    Modules

---
+++ lib.py
@@ -0,0 +1,13 @@
+# Библиотека функций основных математических действий
+def sum(a, b):
+    print(a+b)
+
+
+def diff(a, b):
+    print(a - b)
+
+if __name__ == '__main__':
+    print('Последняя строка модуля lib')
+else:
+    print('__name__')
+
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-# Исключения try+exept / try+finally
 # Модули и их применение
-# Утверждения (Assertion) используется на момент отладки программы
+#import lib as l
 
-try:
-    text = input('Введите любой текст: ')
-    assert len(text) > 3  # утверждение
-except AssertionError:
-    print('Длина текста меньше 3 символов')
+#if __name__ as '__main__':
+    #l.sum(5, 5)
+
+# from lib import *  перегружает пространство имен
+# from lib import sum  перегружает пространство имен
+# sum(5, 5)
+
+import requests
+
+key = 'dc3659c8cdf392c6f36d48a66fd18765'
+city = input('Введите город: ')
+
+# ссылка, с которой мы получим все данные в формате JSON
+url = 'http://api.openweathermap.org/data/2.5/weather'
+# Дополнительные параметры (Ключ, город введенный пользователем и единицы измерения - metric означает Цельсий)
+params = {'APPID': key, 'q': city, 'units': 'metric'}
+
+result = requests.get(url, params=params)
+res = result.json()
+print(res)
+print('Температура:', res['main']['temp'], '°C')
+print('Реально ощущается как:', res['main']['feels_like'], '°C')
