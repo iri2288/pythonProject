51772114f7d082419caf10ce0912314de4470a4f
iri2288 <133140596+iri2288@users.noreply.github.com>
2023-09-03 12:25:32 +0300

    Placeholder format string

--- main.py
+++ main.py
@@ -2,14 +2,32 @@
 # Исключения
 # Модули и их применение
 # Поиск строки
-# Метод find(substr, start=0, end = None)
+# Метод replace(old, new, count = None)
+# Форматирование строки - позволяет выводить строку в удобном формате
+# %s - строка, %f - дробь, %d - целое placeholder
 
-string = 'Видеть, петь'
-if 'еть' in string:
-    count = string.count('еть')
-    print('"еть" встречается ', count, 'раз(а):')
+string = '+7-812-345-67-89'
+lenght = len(string)
 
-if string.find('еть') != -1:
-    count = string.count('еть')
-    print('"еть" встречается ', count, 'раз(а):')
+# заменить дефис на пробел
+
+spaced_phone = string.replace('-', ' ') # новая переменная тк строка неизменяемый тип данных
+print(spaced_phone)
+
+open_braked_phone = string.replace('-', ' (', 1)
+close_braked_phone = open_braked_phone.replace('-', ') ', 1)
+print(close_braked_phone)
+
+tv = 'тиливизор'  # телевизор
+write_tv = tv.replace('и', 'е', 2)
+print(write_tv)
+
+# Форматирование строки
+name = 'Виктор'
+age = 9
+height = 141.56
+
+f_string = '%15s,\nвозраст: %d лет,\nрост: %6.1f см' % (name, age, height)
+print(f_string)  # 1f в росте округление
+
 
