5f25e879fd9e738c60262404cea9a366b0aed01d
iri2288 <133140596+iri2288@users.noreply.github.com>
2023-09-08 19:47:30 +0300

    RegExp5

--- main.py
+++ main.py
@@ -10,16 +10,57 @@
 # ? - от единицы до "бесконечности" (32767) 2 байтовый int {1,}
 import re
 
-# найти в тексте номер телефона начинающийся с +7 и содержит 10 цифр
-pattern = r'\+7\d{10}'  # вычленяет 10 цифр, если цифр меньше то возвращает пустоту
-testString = 'Google: +78121234567'
+pattern = r'<img.*>'  # greedy (жадный квантификатор)
+testString = 'Картинка <img src="bg.jpg"> в тексте </p>'
 
 result = re.findall(pattern, testString, re.I)  # re.I игнорируя регистр
 print(result)
 
-pattern = r'стеклянн?ый'  # 2-я "н" может присутствовать но не обязана
-testString = 'Стекляный, стеклянный, оловянный, деревяннный, серебряный'
+pattern = r'<img.*?>'  # minor (не жадный квантификатор)
+testString = 'Картинка <img src="bg.jpg"> в тексте </p>'
 
 result = re.findall(pattern, testString, re.I)  # re.I игнорируя регистр
 print(result)
 
+
+pattern = r'<img[^>]+src="([^">]+)"'  # вычленяем само название файла картинки
+testString = 'Картинка <img src="bg.jpg"> в тексте </p>'
+
+result = re.findall(pattern, testString, re.I)  # re.I игнорируя регистр
+print(result[0])
+
+# в html - документе вытащить, находящееся между <p> и </p>
+pattern = r'<p>(.*?)</p>'  # вычленяем само название файла картинки
+testString = '<b>Жирный </b>.<p>Содержимое абзаца 2</p>.<i>курсив</i>'
+
+result = re.findall(pattern, testString, re.I)  # re.I игнорируя регистр
+print(result[0])
+
+# игнорируя от <p и до >
+pattern = r'<p[^>]*>(.*?)</p>'  # вычленяем само название файла картинки
+testString = '<b>Центрируем: </b><p align="center">Содержимое абзаца </p>'
+
+result = re.findall(pattern, testString, re.I)  # re.I игнорируя регистр
+print(result[0])
+
+# в html вытащить url из текста
+pattern = r'http(?:s)?://[\S]+'  # вычленяем сайт для http
+testString = 'Информация есть на сайте http://google.com'
+
+result = re.findall(pattern, testString)  # re.I игнорируя регистр
+print(result[0])
+
+# https://regex101.com/
+# в html вытащить anchor из тега ссылки
+pattern = r'href=[\'"]?([^\'">]+)'  # вычленяем сайт для http
+testString = '<a href="http://ya.ru">Яндекс</a>'
+
+result = re.findall(pattern, testString)  # re.I игнорируя регистр
+print(result[0])
+
+# в html вытащить anchor из тега ссылки
+pattern = r'<a.*?>(.*?)</a>'  # вычленяем сайт для http
+testString = '<a href="http://ya.ru">Яндекс</a>'
+
+result = re.findall(pattern, testString)  # re.I игнорируя регистр
+print(result[0])
